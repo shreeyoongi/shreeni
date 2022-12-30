78c4c19c1450f57719a673730977f34bb2cd21b5
sowmiya <121333190+shreeyoongi@users.noreply.github.com>
2022-12-30 13:50:43 +0530

    Add files via upload

---
+++ salary output.py
@@ -0,0 +1,8 @@
+Python 3.5.3 (v3.5.3:1880cb95a742, Jan 16 2017, 15:51:26) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:/Users/test02/Desktop/shree salary input.py ===========
+enter the salary:18000
+bonus: 1800.0
+total; 19800.0
+>>> 
---
+++ shree salary input.py
@@ -0,0 +1,11 @@
+salary=int(input('enter the salary:'))
+if(salary>1000):
+    bonus=salary*(10/100)
+    print('bonus:',bonus)
+    print('total;',salary+bonus)
+else:
+    salary<1000
+    bonus=salary*(12/100)
+    print('bonus:',bonus)
+    print('total;',salary+bonus)
+    
